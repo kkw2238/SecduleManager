a11e60b2a59fea2fa9f1afb41c40cc1a3c1678d4
kkw2238 <kkw2238@naver.com>
2024-08-14 15:06:59 +0900

    init: 프로젝트 생성
    sql비밀번호와 같은 민감 정보가 들어있는
    application.properties는 제외

---
+++ src/main/java/com/sparta/secdulemanager/SecduleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.secdulemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecduleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecduleManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/secdulemanager/SecduleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.secdulemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecduleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

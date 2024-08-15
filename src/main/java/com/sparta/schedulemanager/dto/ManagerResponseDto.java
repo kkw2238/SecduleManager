66063cb1ee8a9db8710ca5bd0fada7c8a02be701
kkw2238 <kkw2238@naver.com>
2024-08-16 00:43:08 +0900

    fix: 매니저 추가시 id도 반환하게 수정

--- src/main/java/com/sparta/schedulemanager/dto/ManagerResponseDto.java
+++ src/main/java/com/sparta/schedulemanager/dto/ManagerResponseDto.java
@@ -7,12 +7,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ManagerResponseDto {
+    private Integer Id;
     private String managerName;
     private String managerEmail;
     private String editTime;
     private String createTime;
 
     public ManagerResponseDto(Manager manager) {
+        this.Id = manager.getId();
         this.managerName = manager.getManagerName();
         this.managerEmail = manager.getManagerEmail();
         this.editTime = manager.getEditTime();
